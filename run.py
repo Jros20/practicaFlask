673d73b029a1d7801460cacafd61f99e73ea0c33
Jros20 <juanantonioros20@gmail.com>
2024-04-02 16:46:42 +0200

    metodo post satisfactorio

--- run.py
+++ run.py
@@ -1,10 +1,15 @@
-from flask import Flask , render_template
+from flask import Flask, render_template, request
 #Crear app medante instancia
 app = Flask(__name__)
 #Crear rutas con sus correspondientes funciones
-@app.route('/',methods=['GET'])
-def holamundo():
-    return render_template('index.html')
+@app.route('/', methods=['GET', 'POST'])
+def index():
+ if request.method == 'POST':
+        nombre = request.form['Nombre']
+        return render_template('/index.html', nombre = nombre)
+ else:
+        return render_template('/index.html')
+ 
 @app.route('/mis-proyectos')
 def mostrarproyectos():
     return 'Aquí se mostrarán mis proyectos'
