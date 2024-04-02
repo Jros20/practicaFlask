ebde1dd4076621d906ca13ce9f3338775e673553
Jros20 <juanantonioros20@gmail.com>
2024-04-02 17:02:25 +0200

    vuelta a la practica

--- run.py
+++ run.py
@@ -1,18 +1,15 @@
-from flask import Flask, render_template, request
-#Crear app medante instancia
-app = Flask(__name__)
-#Crear rutas con sus correspondientes funciones
-@app.route('/', methods=['GET', 'POST'])
-def index():
- if request.method == 'POST':
-        nombre = request.form['Nombre']
-        return render_template('/index.html', nombre = nombre)
- else:
-        return render_template('/index.html')
- 
-@app.route('/mis-proyectos')
-def mostrarproyectos():
-    return 'Aquí se mostrarán mis proyectos'
-#Ejecutar nuestra app cuando ejecutemos este archivo run.py
-if __name__ == '__main__':
-    app.run(debug=True)
+from flask import Flask, request, render_template #Importamos clase Flask, objeto request y función render_template de flask (librería)
+app = Flask(__name__) #Creamos una app instanciando la clase Flask (automáticamente el nombre de la app)
+#Rutas - (argumentos: Url) - Función
+@app.route('/', methods = ['GET', 'POST']) #Decoramos con método de app que es una instancia de la clase Flask y argumentamos "slash"
+def inicio(): #Definimos una función llamada Inicio
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template('/index.html') #Retornornamos el template usando RENDER_TEMPLATE()
+@app.route('/agradecimiento', methods = ['GET']) #Decoramos con método routes la próxima función arguementando la url "/agradecimiento"
+def agradecer(): #Definimos una función llamada agradecer
+    return 'Gracias pythones.net!' #Retorno de la función
+#Iniciar app
+if __name__ == '__main__': #Condicional de que si la aplicación ejecutada se coincide al nombre de la aplicación
+    app.run('127.0.0.1', 5000, debug=True) #Método que inicia la app con la dirección, puertos y modo de argumentos
