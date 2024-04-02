9dca24f565d658cbc2596e975897b1f35388c5d1
Jros20 <juanantonioros20@gmail.com>
2024-04-02 15:32:26 +0200

    Hemos añadido nuevas rutas y nuevos templates vacios para esas rutas

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask , render_template
 #Crear app medante instancia
 app = Flask(__name__)
 #Crear rutas con sus correspondientes funciones
 @app.route('/',methods=['GET'])
 def holamundo():
-    return 'Hola Mundo!'
+    return render_template('index.html')
 @app.route('/mis-proyectos')
 def mostrarproyectos():
     return 'Aquí se mostrarán mis proyectos'
